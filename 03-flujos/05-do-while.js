562d05a124d0de2930c808e2fb53a201681b325e
limferth <limferth@gmail.com>
2024-05-09 23:05:19 -0400

    aprendimos el do while

---
+++ 03-flujos/05-do-while.js
@@ -0,0 +1,10 @@
+// aprendiendo sobre el loop DO WHILE
+// la diferencia radica en que en lugar de evaluar la condicion al principo lo vab hacer al final.
+let i = 2;
+do {
+	if (i % 2 == 0) {
+		console.log("numero par", i);
+	}
+	i++;
+} while(i < 2);
+
