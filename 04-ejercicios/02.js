30bbe3c4def7b0fec276122a708fdf116e4da84e
limferth <limferth@gmail.com>
2024-05-11 12:15:42 -0400

    aprendiendo el segundo ejercicio

---
+++ 04-ejercicios/02.js
@@ -0,0 +1,20 @@
+// EJERCICIO NUMERO 2
+
+function nombreResolucion(ancho, alto) {
+	if (ancho >= 7680 && alto >= 4320) {
+		return '8k';
+	} else if (ancho >= 3840 && alto >= 2160) {
+		return '4k';
+	} else if (ancho >= 2560 && alto  >= 1440) {
+	return 'WQHD';
+	} else if (ancho >= 1920 && alto >= 1080) {
+		return 'FHD';
+	} else if (ancho >= 1280 && alto >= 720) {
+		return 'hd';
+	} else {
+		return false;
+	}
+};
+
+let nombre = nombreResolucion(7840, 3104);
+console.log(nombre);
