f35e546db59f34f4d0b72fc5093e431186fc2857
limferth <limferth@gmail.com>
2024-05-10 20:45:04 -0400

    aprendimos lo que es for of de js

---
+++ 03-flujos/07-for-of.js
@@ -0,0 +1,16 @@
+// FOR OF
+
+
+let animales = ["chanchito feliz", "Dragon", "Perrito"];
+
+for (let animal of animales) {
+	console.log(animal);
+}
+
+// de manera manual  
+//
+let i = 0;
+while (i < animales.length) {
+	console.log(animales[i]);
+	i++;
+}
