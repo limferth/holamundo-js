d192b25f518a67e7a556eb2cd6bea5486a64a88f
limferth <limferth@gmail.com>
2024-05-10 21:24:02 -0400

    aprendimos el loop switch

---
+++ 03-flujos/10-switch.js
@@ -0,0 +1,15 @@
+// SWITCH COMO FUNCIONA Y COMO UTILIZARLO
+
+let accion = 'listar';
+
+switch (accion) {
+	case 'listar':
+		console.log('accion de listar');
+		break;
+	case 'guardar':
+		console.log('accion de guardar');
+		break;
+	default:
+		consoloe.log('accion no reconocida')
+}
+
