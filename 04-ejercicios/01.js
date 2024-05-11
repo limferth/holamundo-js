0393f0d23dd162858f89807052e78dd7198b516e
limferth <limferth@gmail.com>
2024-05-11 11:42:09 -0400

    completamos el ejercicio nro 1

--- 04-ejercicios/01.js
+++ 04-ejercicios/01.js
@@ -1,9 +1,23 @@
 // EJERCICIO NUMERO UNO
 
 function cualEsMayor(a, b) {
+	if (a > b) {
+		return a;
+	} else {
+		return b;
+	}
+	
 }
 
 
-let mayor = CualEsMayor(10, 5);
+let mayor = cualEsMayor(10, 5);
 
-console.log(mayor);
+console.log(mayor); 
+
+function soyMayor(a, b) {
+	return (a > b) ? a:b;
+}
+
+let major = soyMayor(18, 16);
+
+console.log('soy mayor de edad', major);
