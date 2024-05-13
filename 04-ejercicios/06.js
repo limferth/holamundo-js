ac6732e30d4ecdd71193be122ef271e411d57bf5
limferth <limferth@gmail.com>
2024-05-12 23:01:51 -0400

    completamamos los ejecicios totales

---
+++ 04-ejercicios/06.js
@@ -0,0 +1,18 @@
+// ejercicio numero 6
+// crear algoritmo que devuelva cantidad de numeros positivos de un array
+
+
+let array = [2, 5, 7, 15, -5, -100, 55];
+
+function cuantosPositivos(arr) {
+	let cantidad = 0;
+	for (elemento of arr) {
+		if (elemento > 0) {
+			cantidad++;
+		}
+	}
+	return cantidad;
+}
+
+let resultado = cuantosPositivos(array);
+console.log(resultado);
